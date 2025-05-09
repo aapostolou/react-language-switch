97774ba4b7bfda2a5722a9d2d89b88fe5072caae
aapostolou <alexandros.apostolou@outlook.com>
2025-05-09 17:35:08 +0300

    Fixed CSS injection in JS

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import {resolve} from 'path';
+import hq from "alias-hq";
+import { resolve } from "path";
+import postcssPresetEnv from "postcss-preset-env";
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import dts from "vite-plugin-dts";
 
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
-import hq from 'alias-hq';
-import external from '@yelo/rollup-node-external';
-import dts from 'vite-plugin-dts';
-import postcssPresetEnv from 'postcss-preset-env';
+import react from "@vitejs/plugin-react";
+import external from "@yelo/rollup-node-external";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
-        alias: hq.get('rollup'),
+        alias: hq.get("rollup"),
     },
-    plugins: [react(), dts({rollupTypes: true, exclude: ['**/*.stories.(ts|tsx)']})],
+    plugins: [react(), dts({ rollupTypes: true, exclude: ["**/*.stories.(ts|tsx)"] }), cssInjectedByJsPlugin()],
     build: {
         sourcemap: true,
         lib: {
             // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'src/lib/index.ts'),
-            name: 'Library name',
+            entry: resolve(__dirname, "src/lib/index.ts"),
+            name: "Library name",
             // the proper extensions will be added
-            fileName: 'index',
+            fileName: "index",
         },
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
@@ -30,17 +31,17 @@ export default defineConfig({
                 // Provide global variables to use in the UMD build
                 // for externalized deps
                 globals: {
-                    react: 'React',
+                    react: "React",
                 },
             },
         },
     },
     css: {
         modules: {
-            localsConvention: 'camelCase',
+            localsConvention: "camelCase",
         },
         postcss: {
-            plugins: [postcssPresetEnv({stage: 1})],
+            plugins: [postcssPresetEnv({ stage: 1 })],
         },
     },
 });
